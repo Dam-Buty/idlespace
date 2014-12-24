d230d02d54cb3a161f376d4737df1402bbe19af5
Damien BUTY <dam.buty@gmail.com>
2014-12-24 11:35:47 +0100

    Collisions and shield OK

--- js/idlespace.js
+++ js/idlespace.js
@@ -3,22 +3,23 @@ var enemies = [{
     name: "Alice",
     hp: 10,
     scrap: [1, 3],
-    population: 8,
-    sectors: [-20, 10],
-    attack: 10,
-    defense: 5
+    population: 25,
+    sectors: [-20, 30],
+    shield: 5
 }, {
     name: "Bob",
     hp: 18,
     scrap: [2, 4],
     population: 8,
-    sectors: [-20, 10]
+    sectors: [-20, 50],
+    shield: 10
 }, {
     name: "Dave",
     hp: 42,
     scrap: [5, 10],
     population: 4,
-    sectors: [12, 25]
+    sectors: [-20, 80],
+    shield: 20
 }];
 
 var Utils = {
@@ -104,11 +105,10 @@ var Utils = {
     },
     
     collision: function(el, targets, max) {
-        function getPositions(el) {
-            var sprite = el.sprite;            
+        function getPositions(el) {          
             return [ 
-                [sprite.offsetLeft, sprite.offsetLeft + el.width], 
-                [sprite.offsetTop, sprite.offsetTop + el.height] 
+                [el.left, el.left + el.width], 
+                [el.top, el.top + el.height] 
             ];
         }
 
@@ -119,18 +119,18 @@ var Utils = {
             return r1[1] > r2[0] || r1[0] === r2[0];
         }
         
-        var shot = [];        
+        var collisions = [];        
         var elPos = getPositions(el);
         
-        for(var i = 0;i < targets.length && (shot.length < max || targets.length);i++) {
+        for(var i = 0;i < targets.length && (collisions.length < (max || targets.length));i++) {
             var targetPos = getPositions(targets[i]);
             
             if (comparePositions(elPos[0], targetPos[0]) && comparePositions(elPos[1], targetPos[1])) {
-                shot.push(targets[i]);
+                collisions.push(targets[i]);
             }
         }
         
-        return shot;
+        return collisions;
     }
 };
 
@@ -140,7 +140,7 @@ var Game = {
     
     grid: {
         x: 100,
-        y: 30
+        y: 50
     },
     chunk: {
         x: 0,
@@ -158,8 +158,8 @@ var Game = {
     
     hudArea: {
         element: undefined,
-        
-        gameSector: undefined
+        gameSector: undefined,
+        scrap: undefined
     },
     
     enemies: {
@@ -193,14 +193,19 @@ var Game = {
             this.live.splice(idx, 1);
             if (this.live.length == 0) {
                 Game.nextSector();
+            } else {
+                this.reorder();
+            }
+        },
+        
+        reorder: function() {
+            for(var i = 0;i < this.live.length;i++) {
+                this.live[i].idx = i;
             }
         }
     },
     
-    go: function() {
-        this.spawner = Spawner();
-        this.ship = Ship();
-        
+    go: function() {        
         // Init game Area
         this.gameArea.element = document.getElementById("game-area");
         this.gameArea.width = this.gameArea.element.offsetWidth;
@@ -209,12 +214,16 @@ var Game = {
         // Init HUD
         this.hudArea.element = document.getElementById("hud-area");
         this.hudArea.gameSector = document.getElementById("game-sector");
+        this.hudArea.shipLives = document.getElementById("ship-lives");
+        this.hudArea.scrap = document.getElementById("ship-scrap");
         
         // Calculate grid
         this.chunk.x = this.gameArea.width / this.grid.x;
         this.chunk.y = this.gameArea.height / this.grid.y;
         
-        // Spawn ship
+        // Init spawner and ship
+        this.spawner = Spawner();
+        this.ship = Ship();
         this.gameArea.element.appendChild(this.ship.sprite);
         
         // Map keyboard
@@ -229,6 +238,28 @@ var Game = {
         this.enemies.spawn();
         
         this.hudArea.gameSector.innerHTML = this.sector;
+    },
+    
+    scrap: function(scrap, delay, coords) {
+        var values = [1000, 500, 100, 50, 10, 5, 1];
+        for (;scrap > 0;) {
+            for(var i = 0;i < values.length;i++) {
+                var value = values[i];
+                var top = coords.top + (Math.random() * coords.height);
+                var left = coords.left + (Math.random() * coords.width);
+            
+                if (scrap >= value) {
+                    Scrap({
+                        top: top,
+                        left: left,
+                        delay: delay,
+                        value: value
+                    });
+                    scrap -= value;
+                    break;
+                }
+            }
+        }
     }
 };
 
@@ -236,7 +267,7 @@ var Spawner = function() {
     return {
         enemies: enemies,
         minDelay: 100,
-        maxDelay: 600,
+        maxDelay: 300,
         
         isEligible: function(enemy) {
             return (enemy.sectors[0] <= Game.sector && Game.sector <= enemy.sectors[1]);
@@ -277,26 +308,54 @@ var Spawner = function() {
 
 var Ship = function() {
     var sprite = Utils.sprite("ship");
+    
     var width = sprite.offsetWidth;
     var height = sprite.offsetHeight;
+    var top = Game.gameArea.height / 2;
+    var left = 0;
+    
+    var clone = sprite.cloneNode(true);
+    var hudHp = clone.getElementsByClassName("hp")[0];
+    var hudShield = clone.getElementsByClassName("shield")[0];
     
     return {
-        hp: 10,
         lives: 3,
+        hp: 30,
+        shield: 20,
+        maxHP: 30,
+        maxShield: 30,
+        scrap: 0,
         
-        sprite: sprite.cloneNode(true),
+        sprite: clone,
         width: width,
         height: height,
+        top: top,
+        left: left,
+        
+        hud: {
+            hp: hudHp,
+            shield: hudShield
+        },
         
         systems: {   
             thrusters: {
                 speed: 2
             },
             
+            shieldRepair: {
+                time: 500,
+                amount: 10
+            },
+            
             autoRepair: {
-                active: true,
+                active: false,
                 time: 2000,
                 lives: 1
+            },
+            
+            magnet: {
+                radius: 50,
+                force: 10
             },
             
             weapons: {
@@ -331,18 +390,58 @@ var Ship = function() {
         move: function(direction) {
             switch(direction) {
                 case "left":
-                    this.sprite.style.left = Math.max(0, this.sprite.offsetLeft - (Game.chunk.x * this.systems.thrusters.speed));
+                    this.left = Math.max(0, this.left - (Game.chunk.x * this.systems.thrusters.speed));
                     break;
                 case "up":
-                    this.sprite.style.top = Math.max(0, this.sprite.offsetTop - (Game.chunk.y * this.systems.thrusters.speed));
+                    this.top = Math.max(0, this.top - (Game.chunk.y * this.systems.thrusters.speed));
                     break;
                 case "right":
-                    this.sprite.style.left = Math.min(Game.gameArea.width - this.width, this.sprite.offsetLeft + (Game.chunk.x * this.systems.thrusters.speed));
+                    this.left = Math.min(Game.gameArea.width - this.width, this.left + (Game.chunk.x * this.systems.thrusters.speed));
                     break;
                 case "down":
-                    this.sprite.style.top = Math.min(Game.gameArea.height - this.height, this.sprite.offsetTop + (Game.chunk.y * this.systems.thrusters.speed));
+                    this.top = Math.min(Game.gameArea.height - this.height, this.top + (Game.chunk.y * this.systems.thrusters.speed));
                     break;
             }
+            
+            this.sprite.style.top = this.top;
+            this.sprite.style.left = this.left;
+        },
+        
+        hit: function(damage) {
+            this.hp -= damage;
+            this.shield = Math.max(0, this.shield - damage);
+            
+            if (this.hp <= 0) {
+                this.die();
+            }
+            
+            this.hud.hp.style.width = (this.hp * 100 / this.maxHP) + "%";
+            this.hud.shield.style.width = (this.shield * 100 / this.maxShield) + "%";
+            
+            this.repairShield();
+        },
+        
+        die: function() {
+            this.lives--;
+            this.hp = this.maxHP;
+            this.shield = this.maxShield;
+            Game.hudArea.shipLives.innerHTML = this.lives;
+        },
+        
+        repairShield: function() {
+            var self = this;
+            
+            setTimeout(function() {
+                if (self.shield < self.maxShield) {
+                    self.shield = Math.min(self.maxShield, self.shield + self.systems.shieldRepair.amount);
+                    self.hud.shield.style.width = (self.shield * 100 / self.maxShield) + "%";
+                }
+            }, self.systems.shieldRepair.time);
+        },
+        
+        addScrap: function(value) {
+            this.scrap += value;
+            Game.hudArea.scrap.innerHTML = this.scrap;
         }
     };
 };
@@ -351,25 +450,39 @@ var Enemy = function(enemy) {
     var sprite = Utils.sprite(enemy.name);
     var width = sprite.offsetWidth;
     var height = sprite.offsetHeight;
+    var top = 0;
+    var left = Game.gameArea.width;
+    
+    var clone = sprite.cloneNode(true);
+    var hudHp = clone.getElementsByClassName("hp")[0];
     
     return {
         idx: undefined,
+        handle: undefined,
         
         name: enemy.name,
         maxHP: enemy.hp,
         hp: enemy.hp,
+        shield: enemy.shield,
         
         scrap: Utils.distribution(enemy.scrap),
         delay: Utils.linear([Game.spawner.minDelay, Game.spawner.maxDelay]),
         
-        sprite: sprite.cloneNode(true),
+        sprite: clone,
         width: width,
         height: height,
+        top: top,
+        left: left,
         
+        hud: {
+            hp: hudHp
+        },
+                
         handle: undefined,
         
         spawn: function() {
-            this.sprite.style.top = Math.random() * (Game.gameArea.height - this.height);
+            this.top = Math.random() * (Game.gameArea.height - this.height);
+            this.sprite.style.top = this.top;
             
             this.sprite.style.transitionDuration = this.delay;
 
@@ -379,36 +492,78 @@ var Enemy = function(enemy) {
         },
         
         move: function() {
-            var self = this;
-            
-            this.sprite.style.right = (Game.gameArea.width - this.sprite.offsetLeft - this.width) + Game.chunk.x;
-            
-            if (this.sprite.offsetLeft <= 0) {
-                this.sprite.style.right = 0;
+            if (!this.sprite.classList.contains("exploding")) {
+                var self = this;
+                
+                this.left -= Game.chunk.x;
+                
+                if (this.left < 0) {
+                    this.warp();
+                }
+                
+                this.sprite.style.left = this.left;
+                
+                this.collision();
+                
+                this.handle = setTimeout(function() {
+                    self.move();
+                }, this.delay); 
             }
-            
-            this.handle = setTimeout(function() {
-                self.move();
-            }, this.delay); 
         },
         
-        shotAt: function() {
-            this.hp = this.hp - Game.ship.systems.weapons.damage;
+        warp: function() {
+            var self = this;
             
-            this.sprite.getElementsByClassName("hp")[0].style.width = (this.hp * 100 / this.maxHP) + "%";
+            this.left = Game.gameArea.width;
+            this.sprite.style.transitionDuration = 0;
+            this.sprite.style.left = this.left;
+            
+            setTimeout(function() {
+                self.sprite.style.transitionDuration = self.delay;
+            }, self.delay);
+        },
+        
+        collision: function() {
+            var hit = Utils.collision(this, [Game.ship], 1);
+            
+            if (hit.length > 0) {
+                hit[0].hit(this.shield);
+                this.hit(hit[0].shield);
+            }
+        },
+        
+        hit: function(damage) {
+            this.hp = this.hp - damage;
             
             if (this.hp <= 0) {
                 this.die();
+            } else {
+                this.hud.hp.style.width = (this.hp * 100 / this.maxHP) + "%";
             }
+            
         },
         
         die: function() {
+            var self = this;
+            
             clearInterval(this.handle);
             this.handle = undefined;
             
-            Game.gameArea.element.removeChild(this.sprite);
+            this.sprite.removeChild(this.hud.hp);
+            this.sprite.classList.add("exploding");
             
-            Game.enemies.kill(this.idx);
+            setTimeout(function() {
+                Game.scrap(self.scrap, self.delay, {
+                    width: self.width,
+                    height: self.height,
+                    top: self.top,
+                    left: self.left
+                });
+                
+                Game.gameArea.element.removeChild(self.sprite);
+                
+                Game.enemies.kill(self.idx);
+            }, 1000);
         }
     };
 };
@@ -417,9 +572,11 @@ var Bullet = function(options) {
     var sprite = Utils.sprite("bullet");
     var width = sprite.offsetWidth;
     var height = sprite.offsetHeight;
+    var top = Game.ship.top + (Game.ship.height / 2);
+    var left = Game.ship.left + Game.ship.width;
     
     return {
-        rate: 100,
+        delay: 100,
         handle: undefined,        
         
         hostile: options.hostile,
@@ -430,24 +587,27 @@ var Bullet = function(options) {
         sprite: sprite.cloneNode(true),
         width: width,
         height: height,
+        top: top,
+        left: left,
         
         shoot: function() {
             var self = this;
             
-            this.sprite.style.top = Game.ship.sprite.offsetTop + (Game.ship.height / 2);
-            this.sprite.style.left = Game.ship.sprite.offsetLeft + Game.ship.width;
+            this.sprite.style.top = this.top;
+            this.sprite.style.left = this.left;
             
             Game.gameArea.element.appendChild(this.sprite);
             
             this.handle = setInterval(function() {
                 self.move();
-            }, this.rate);
+            }, this.delay);
         },
         
         move: function() {
-            this.sprite.style.left = this.sprite.offsetLeft + (Game.chunk.x * this.speed);
+            this.left = this.left + (Game.chunk.x * this.speed);
+            this.sprite.style.left = this.left;
             
-            if (this.collision() || this.sprite.offsetLeft >= (Game.gameArea.width - this.width)) {
+            if (this.collision() || this.left >= (Game.gameArea.width - this.width)) {
                 this.die();
             }
         },
@@ -456,7 +616,7 @@ var Bullet = function(options) {
             var shot = Utils.collision(this, Game.enemies.live, 1);
             
             if (shot.length > 0) {
-                shot[0].shotAt();
+                shot[0].hit(this.damage);
                 return true;
             }
             
@@ -470,5 +630,71 @@ var Bullet = function(options) {
         }
     }.shoot();
 };
+
+var Scrap = function(scrap) {
+    var sprite = Utils.sprite("scrap");
+    var width = sprite.offsetWidth;
+    var height = sprite.offsetHeight;
+    
+    return {
+        handle: undefined,
+        delay: scrap.delay,
+        
+        value: scrap.value,
+        
+        sprite: sprite.cloneNode(true),
+        top: scrap.top,
+        left: scrap.left,
+        width: width,
+        height: height,
+        
+        shoot: function() {
+            var self = this;
+            
+            this.sprite.style.top = this.top;
+            this.sprite.style.left = this.left;
+            
+            Game.gameArea.element.appendChild(this.sprite);
+            
+            this.handle = setInterval(function() {
+                self.move();
+            }, this.delay);
+        },
+        
+        move: function() {
+            this.left = this.left - Game.chunk.x;
+            this.sprite.style.left = this.left;
+            
+            if (this.collision() || this.left >= (Game.gameArea.width - this.width)) {
+                this.die();
+            }
+        },
+        
+        collision: function() {
+            var ship = Utils.collision(this, [Game.ship], 1)[0];
+            
+            if (ship !== undefined) {
+                ship.addScrap(this.value);
+                return true;
+            }
+            
+            return false;
+        },
+        
+        die: function() {
+            var self = this;
+            
+            clearInterval(this.handle);
+            this.handle = undefined;
+            this.sprite.innerHTML = this.value;
+            this.sprite.classList.add("scrapping");
+            
+            setTimeout(function() {
+                Game.gameArea.element.removeChild(self.sprite);
+            }, 500);
+        }
+    }.shoot();
+};
+
 
 Game.go();
